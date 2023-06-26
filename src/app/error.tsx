5d625f77deb9debb26727dea3e79cd89f3ffbed6
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2023-06-25 22:29:18 -0400

    optimizations

---
+++ src/app/error.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+// Error components must be Client components
+import { useEffect } from 'react';
+
+export default function Error({
+	error,
+	reset,
+}: {
+    error: Error;
+    reset: () => void;
+}) {
+	useEffect(() => {
+		// Log the error to an error reporting service
+		console.error(error);
+	}, [error]);
+
+	return (
+		<div className=''>
+			<div className='flex content-center justify-center p-4 text-9xl text-red-600'>
+				<i className='bi bi-exclamation-triangle'></i>
+			</div>
+			<h2 className='text-center text-red-600'>Blunder!</h2>
+			<h3 className='text-center text-red-800'>{error.message}</h3>
+			<div className='flex content-center justify-center p-4'>
+				<button
+					className='btn btn-danger'
+					onClick={
+						// Attempt to recover by trying to re-render the segment
+						() => reset()
+					}
+				>
+                    Retry
+				</button>
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import ExampleImage from '../../public/example.jpg'
 import Hero from '@/components/hero'
+import Stats from '@/components/stats'
 
 export default function Home() {
   return (
    <>
       <Hero />
+      <Stats />
    </>
   );
 }
